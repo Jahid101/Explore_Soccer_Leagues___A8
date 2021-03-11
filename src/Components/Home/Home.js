fc1968717e6927d9bd8f3884b0d3c7ac72491fed
Jahid101 <jahidhasananik.official@gmail.com>
2021-03-11 17:41:18 +0600

    gooooooo

---
+++ src/Components/Home/Home.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import Card from '../Card/Card';
+// import banner from '../../photo/Banner.jpg';
+import './Home.css';
+
+const Home = () => {
+
+    const [leagues, setLeagues] = useState([]);
+
+    useEffect(()=>{
+        const url = 'https://www.thesportsdb.com/api/v1/json/1/all_leagues.php';
+        fetch(url)
+        .then(res => res.json())
+        .then(data => setLeagues(data.leagues.slice(0,21)))
+    }, [])
+
+    return (
+        <div>
+            <h1 className="banner bannerName"><strong>Explore Soccer Leagues</strong></h1>
+            {
+                leagues.map(league => <Card league = {league}></Card>)
+            }
+        </div>
+    );
+};
+
+export default Home;
